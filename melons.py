292ae79b9590bc3aec76f0b6f8911c090f6ac81b
Leilah Wang <leilahwang111@gmail.com>
2021-05-06 21:32:53 +0000

    Passed Inspection Complete
    Co-authored by: Crystal Anderson <cryssann@gmail.com>

--- melons.py
+++ melons.py
@@ -10,18 +10,40 @@ class AbstractMelonOrder(): #This of this as BASE Order~
         self.shipped = False 
 
 
+class GovernmentMelonOrder(AbstractMelonOrder):
+        # """Melon Inspection."""
+
+    def __init__(self, species, qty):
+        super().__init__(species, qty)
+        # """Initialize melon order attributes."""
+
+        self.passed_inspection = False
+        
+    def mark_inspection(self):
+        self.passed_inspection = True
+
+
+    def get_total(self):
+        """Calculate price, there's no tax."""
+
+        base_price = 5 
+
+        total = self.qty * base_price
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
     def __init__(self, species, qty):
         super().__init__(species, qty)
         """Initialize melon order attributes."""
-    
-#------------------------------------------------------------------------------
-        # self.species = species
-        # self.qty = qty
-        # self.shipped = False #NOT IN ATTRIBUTES, but needs to be in main?
-#------------------------------------------------------------------------------
 
         self.order_type = "domestic" 
         self.tax = 0.08 
@@ -44,14 +66,8 @@ class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
-        """Initialize melon order attributes."""
         super().__init__(species, qty, country_code)
-        
-#------------------------------------------------------------------------------
-        # self.species = species
-        # self.qty = qty
-        # self.shipped = False #NOT IN ATTRIBUTES, but needs to be in main?
-#------------------------------------------------------------------------------
+        """Initialize melon order attributes."""
 
         self.order_type = "international" #Is this needed after AbstractMelonOrder?
         self.country_code = country_code #++++This is InternationalMelonOrder specific
@@ -60,8 +76,12 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        base_price = 5 * (1.5)
+
         total = (1 + self.tax) * self.qty * base_price
+
+        if self.qty < 10:
+            total += 5
 
         return total
 
@@ -74,3 +94,5 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+
